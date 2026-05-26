98928715a13896c2053f80032d16030bfffcac18
Willsmt <willian_marthins@hotmail.com>
2026-05-26 08:37:00 -0300

    adicionando componentes a barra lateral, criando props para legendas, ativo e contador e estilizando a barra lateral.

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,3 +1,20 @@
-const BarraLateral = () => <aside> barra lateral </aside>
+import FiltroCard from '../components/FiltroCard'
+import * as S from './styles'
+
+const BarraLateral = () => (
+  <S.Aside>
+    <div>
+      <S.Campo type="text" placeholder="Buscar" />
+      <S.Filtros>
+        <FiltroCard legenda="pedentes" contador={2} />
+        <FiltroCard legenda="concluidas" contador={5} />
+        <FiltroCard legenda="urgentes" contador={6} />
+        <FiltroCard legenda="importantes" contador={7} />
+        <FiltroCard legenda="normal" contador={8} />
+        <FiltroCard legenda="todas" contador={9} ativo />
+      </S.Filtros>
+    </div>
+  </S.Aside>
+)
 
 export default BarraLateral

b2637e314ac54fab5e1d0018484cf391811b488f
Willsmt <willian_marthins@hotmail.com>
2026-05-27 13:58:56 -0300

    adiciando estates para os botoes

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,7 +1,44 @@
+import Tarefa from '../components/Tarefa'
+import { Titulo } from '../components/Tarefa/styles'
+import { Container } from './styles'
+
+const tarefas = [
+  {
+    titulo: 'estudar react',
+    descricao: 'baixar fatura',
+    prioridade: 'importante',
+    status: 'pedente'
+  },
+  {
+    titulo: 'pagar conta',
+    descricao: 'baixar fatura',
+    prioridade: 'importante',
+    status: 'pedente'
+  },
+  {
+    titulo: 'ir para academia',
+    descricao: 'baixar fatura',
+    prioridade: 'importante',
+    status: 'pedente'
+  }
+]
+
 const ListaDeTarefas = () => (
-  <main>
-    <p> 2 tarefas marcadas como: &quot;categoria &quot; e &quot;termo&quot;</p>
-  </main>
+  <Container>
+    <p>2 tarefas marcadas como: &quot;categoria &quot; e &quot;termo&quot;</p>
+    <ul>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            prioridade={t.prioridade}
+            status={t.status}
+          />
+        </li>
+      ))}
+    </ul>
+  </Container>
 )
 
 export default ListaDeTarefas
